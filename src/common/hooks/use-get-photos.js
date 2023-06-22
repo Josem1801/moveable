00ffa4bc032a0fde8299911595228f933e2ada8e
josem1801 <josemanuelrosales2@gmail.com>
2023-06-21 22:33:01 -0600

    chore: add env variable

--- src/common/hooks/use-get-photos.js
+++ src/common/hooks/use-get-photos.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+const ENDPOINT = process.env["REACT_APP_ENDPOINT"] ?? "";
 export const UseGetPhotos = () => {
   const [photos, setPhotos] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
   //Endpoint: https://jsonplaceholder.typicode.com/photos
   React.useEffect(() => {
     setIsLoading(true);
-    fetch("https://jsonplaceholder.typicode.com/photos")
+    fetch(ENDPOINT)
       .then((response) => response.json())
       .then((data) => {
         setPhotos(data);
